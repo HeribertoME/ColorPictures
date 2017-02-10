e59a1e7ccc3f79bc676e595711e1f0f86ed4f34c
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2017-02-10 13:29:02 -0600

    Se agregan permisos y se crean los metodos onClick

--- app/src/main/java/com/hmelizarraraz/colorpictures/MainActivity.java
+++ app/src/main/java/com/hmelizarraraz/colorpictures/MainActivity.java
@@ -2,6 +2,8 @@ package com.hmelizarraraz.colorpictures;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void takePhoto(View view) {
+        Toast.makeText(this, "Foto", Toast.LENGTH_SHORT).show();
+    }
+
+    public void takeVideo(View view) {
+        Toast.makeText(this, "Video", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showPhotos(View view) {
+        Toast.makeText(this, "Galeria fotos", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showVideos(View view) {
+        Toast.makeText(this, "Galeria videos", Toast.LENGTH_SHORT).show();
     }
 }
